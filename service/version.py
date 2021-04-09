227bedf76faf2b0e13df75098f04e9512811b618
ermiry <erick.salas@ermiry.com>
2021-04-08 22:53:20 -0500

    Updated version definitions & methods

--- service/version.py
+++ service/version.py
@@ -1,39 +1,39 @@
 import cerver.utils
 
-TODO_VERSION 			= "0.1"encode ('utf-8')
-TODO_VERSION_NAME 		= "Version 0.1"encode ('utf-8')
-TODO_VERSION_DATE 		= "05/04/2021"encode ('utf-8')
-TODO_VERSION_TIME 		= "10:14 CST"encode ('utf-8')
-TODO_VERSION_AUTHOR 	= "Erick Salas"encode ('utf-8')
+TODO_VERSION = "0.1"
+TODO_VERSION_NAME = "Version 0.1"
+TODO_VERSION_DATE = "08/04/2021"
+TODO_VERSION_TIME = "03:10 CST"
+TODO_VERSION_AUTHOR = "Erick Salas"
 
 def todo_version_print_full ():
 	cerver.utils.cerver_log_both (
 		cerver.utils.LOG_TYPE_NONE, cerver.utils.LOG_TYPE_NONE,
-		"\nTodo PyCerver Version: %s".encode ('utf-8'), TODO_VERSION_NAME
+		"\nTodo PyCerver Version: %s".encode ('utf-8'), TODO_VERSION_NAME.encode ('utf-8')
 	)
 
 	cerver.utils.cerver_log_both (
 		cerver.utils.LOG_TYPE_NONE, cerver.utils.LOG_TYPE_NONE,
 		"Release Date & time: %s - %s".encode ('utf-8'),
-		TODO_VERSION_DATE, TODO_VERSION_TIME
+		TODO_VERSION_DATE.encode ('utf-8'), TODO_VERSION_TIME.encode ('utf-8')
 	)
 
 	cerver.utils.cerver_log_both (
 		cerver.utils.LOG_TYPE_NONE, cerver.utils.LOG_TYPE_NONE,
 		"Author: %s\n".encode ('utf-8'),
-		TODO_VERSION_AUTHOR
+		TODO_VERSION_AUTHOR.encode ('utf-8')
 	)
 
 def todo_version_print_version_id ():
 	cerver.utils.cerver_log_both (
 		cerver.utils.LOG_TYPE_NONE, cerver.utils.LOG_TYPE_NONE,
 		"\nTodo PyCerver Version ID: %s\n".encode ('utf-8'),
-		TODO_VERSION
+		TODO_VERSION.encode ('utf-8')
 	)
 
 def todo_version_print_version_name ():
 	cerver.utils.cerver_log_both (
 		cerver.utils.LOG_TYPE_NONE, cerver.utils.LOG_TYPE_NONE,
 		"\nTodo PyCerver Version: %s\n".encode ('utf-8'),
-		TODO_VERSION_NAME
+		TODO_VERSION_NAME.encode ('utf-8')
 	)
