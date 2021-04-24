4c0590a2df805f29de068d8c338dc8e120c84531
ermiry <erick.salas@ermiry.com>
2021-04-23 23:50:59 -0500

    Refactored how main values are got

--- service/todo.py
+++ service/todo.py
@@ -4,48 +4,41 @@ import cerver
 
 from runtime import *
 
-RUNTIME = RUNTIME_TYPE_NONE
+RUNTIME = runtime_from_string (os.environ.get ("RUNTIME"))
 
-PORT = 5000
+PORT = int (os.environ.get ("PORT"))
 
-CERVER_RECEIVE_BUFFER_SIZE = cerver.CERVER_DEFAULT_RECEIVE_BUFFER_SIZE
-CERVER_TH_THREADS = cerver.CERVER_DEFAULT_POOL_THREADS
-CERVER_CONNECTION_QUEUE = cerver.CERVER_DEFAULT_CONNECTION_QUEUE
+CERVER_RECEIVE_BUFFER_SIZE = int (os.environ.get ("CERVER_RECEIVE_BUFFER_SIZE"))
+CERVER_TH_THREADS = int (os.environ.get ("CERVER_TH_THREADS"))
+CERVER_CONNECTION_QUEUE = int (os.environ.get ("CERVER_CONNECTION_QUEUE"))
 
-MONGO_APP_NAME = None
-MONGO_DB = None
-MONGO_URI = None
+MONGO_APP_NAME = os.environ.get ("MONGO_APP_NAME")
+MONGO_DB = os.environ.get ("MONGO_DB")
+MONGO_URI = os.environ.get ("MONGO_URI")
 
-PRIV_KEY = None
-PUB_KEY = None
+PRIV_KEY = os.environ.get ("PRIV_KEY")
+PUB_KEY = os.environ.get ("PUB_KEY")
 
-def todo_init ():
-	global RUNTIME
+ENABLE_USERS_ROUTES = os.environ.get ("ENABLE_USERS_ROUTES")
+if (ENABLE_USERS_ROUTES == "TRUE"):
+	ENABLE_USERS_ROUTES = True
+else:
+	ENABLE_USERS_ROUTES = False
 
-	global PORT
+def todo_config ():
+	print ("RUNTIME: ", runtime_to_string (RUNTIME))
 
-	global CERVER_RECEIVE_BUFFER_SIZE
-	global CERVER_TH_THREADS
-	global CERVER_CONNECTION_QUEUE
+	print ("PORT: ", PORT)
 
-	global MONGO_APP_NAME
-	global MONGO_DB
-	global MONGO_URI
+	print ("CERVER_RECEIVE_BUFFER_SIZE: ", CERVER_RECEIVE_BUFFER_SIZE)
+	print ("CERVER_TH_THREADS: ", CERVER_TH_THREADS)
+	print ("CERVER_CONNECTION_QUEUE: ", CERVER_CONNECTION_QUEUE)
 
-	global PRIV_KEY
-	global PUB_KEY
+	print ("MONGO_APP_NAME: ", MONGO_APP_NAME)
+	print ("MONGO_DB: ", MONGO_DB)
+	print ("MONGO_URI: ", MONGO_URI)
 
-	RUNTIME = runtime_from_string (os.environ.get ("RUNTIME"))
+	print ("PRIV_KEY: ", PRIV_KEY)
+	print ("PUB_KEY: ", PUB_KEY)
 
-	PORT = int (os.environ.get ("PORT"))
-
-	CERVER_RECEIVE_BUFFER_SIZE = int (os.environ.get ("CERVER_RECEIVE_BUFFER_SIZE"))
-	CERVER_TH_THREADS = int (os.environ.get ("CERVER_TH_THREADS"))
-	CERVER_CONNECTION_QUEUE = int (os.environ.get ("CERVER_CONNECTION_QUEUE"))
-
-	MONGO_APP_NAME = os.environ.get ("MONGO_APP_NAME")
-	MONGO_DB = os.environ.get ("MONGO_DB")
-	MONGO_URI = os.environ.get ("MONGO_URI")
-
-	PRIV_KEY = os.environ.get ("PRIV_KEY")
-	PUB_KEY = os.environ.get ("PUB_KEY")
+	print ("ENABLE_USERS_ROUTES: ", ENABLE_USERS_ROUTES)
