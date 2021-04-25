f2d179e11617580b5c7f28d8c0fc26895e48b523
ermiry <erick.salas@ermiry.com>
2021-04-24 23:48:35 -0500

    Updated items handlers & added update handler

--- service/routes/items.py
+++ service/routes/items.py
@@ -43,7 +43,7 @@ def todo_item_create_handler (http_receive, request):
 
 	body = cerver.http_request_get_body (request)
 
-	result = controllers.items.todo_item_create (body, user)
+	result = controllers.items.todo_item_create (user, body)
 
 	if result == TODO_ERROR_NONE:
 		response = cerver.http_response_json_msg (
@@ -72,7 +72,7 @@ def todo_item_get_handler (http_receive, request):
 		request, 0
 	)
 
-	result = controllers.items.todo_item_get_by_id_and_user (
+	result = controllers.items.todo_item_get_by_id_and_user_to_json (
 		item_id_str, user
 	)
 
@@ -81,11 +81,50 @@ def todo_item_get_handler (http_receive, request):
 			http_receive, result.encode ("utf-8"), len (result)
 		)
 
+	else:
+		response = cerver.http_response_json_error (
+			cerver.HTTP_STATUS_NOT_FOUND,
+			"Item not found".encode ('utf-8')
+		)
+
+		if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
+			cerver.http_response_print (response)
+
+		cerver.http_response_send (response, http_receive)
+		cerver.http_response_delete (response)
+
 # PUT /api/todo/items/:id/update
 # a user wants to update an existing item
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def todo_item_update_handler (http_receive, request):
-	pass
+	user = controllers.users.todo_user_load_from_decoded_data (
+		cerver.http_request_get_decoded_data (request)
+	)
+
+	item_id_str = cerver.http_request_get_param_at_idx (
+		request, 0
+	)
+
+	body = cerver.http_request_get_body (request)
+
+	result = controllers.items.todo_item_update (
+		item_id_str, user, body
+	)
+
+	if result == TODO_ERROR_NONE:
+		response = cerver.http_response_json_msg (
+			cerver.HTTP_STATUS_OK,
+			"Updated item!".encode ('utf-8')
+		)
+
+		if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
+			cerver.http_response_print (response)
+
+		cerver.http_response_send (response, http_receive)
+		cerver.http_response_delete (response)
+
+	else:
+		todo_error_send_response (result, http_receive)
 
 # DELETE /api/todo/items/:id/remove
 # deletes an existing user's item
