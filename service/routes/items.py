6a0b4e2710e88da6ba9d727ee905b5411ed421bc
ermiry <erick.salas@ermiry.com>
2021-04-24 23:04:33 -0500

    Added base item create handler logic

--- service/routes/items.py
+++ service/routes/items.py
@@ -3,6 +3,10 @@ import json
 
 import cerver
 
+from errors import *
+import runtime
+import todo
+
 import controllers.items
 import controllers.users
 
@@ -17,15 +21,14 @@ def todo_items_handler (http_receive, request):
 	result = controllers.items.todo_items_get_all_by_user (user)
 	if (result is not None):
 		cerver.http_response_render_json (
-			http_receive, result, len (result)
+			http_receive, result.encode ("utf-8"), len (result)
 		)
 
 	else:
-		items = {
+		response = json.dumps ({
 			"items": []
-		}
+		})
 
-		response = json.dumps (items)
 		cerver.http_response_render_json (
 			http_receive, response.encode ("utf-8"), len (response)
 		)
@@ -34,7 +37,28 @@ def todo_items_handler (http_receive, request):
 # a user has requested to create a new item
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def todo_item_create_handler (http_receive, request):
-	pass
+	user = controllers.users.todo_user_load_from_decoded_data (
+		cerver.http_request_get_decoded_data (request)
+	)
+
+	body = cerver.http_request_get_body (request)
+
+	result = controllers.items.todo_item_create (body, user)
+
+	if result == TODO_ERROR_NONE:
+		response = cerver.http_response_json_msg (
+			cerver.HTTP_STATUS_OK,
+			"Created a new item!".encode ('utf-8')
+		)
+
+		if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
+			cerver.http_response_print (response)
+
+		cerver.http_response_send (response, http_receive)
+		cerver.http_response_delete (response)
+
+	else:
+		todo_error_send_response (result, http_receive)
 
 # GET /api/todo/items/:id/info
 # returns information about an existing item that belongs to a user
