73a4e443c4a0343df959a36e6a66862fad8fbdeb
ermiry <erick.salas@ermiry.com>
2021-04-24 21:21:57 -0500

    Added new methods in user model

--- service/models/user.py
+++ service/models/user.py
@@ -1,15 +1,43 @@
-class User ():
-	def __init__ (self, id, iat, email, name, username):
-		self.id = id
+users = None
 
-		self.email = email
-		self.name = name
-		self.username = username
+def user_model_init (db):
+	global users
+	users = db["users"]
+
+class User ():
+	def __init__ (self):
+		self.id = None
+
+		self.email = None
+		self.name = None
+		self.username = None
 		self.password = None
 
-		self.iat = iat
+		self.iat = 0
 
 		self.items_count = 0
 
 	def __str__ (self):
 		return f'User: \n\t{self.id} \n\t{self.email} \n\t{self.name} \n\t{self.username}'
+
+def user_create (email, name, username, password=None):
+	user = User ()
+	user.email = email
+	user.name = name
+	user.username = username
+	user.password = password
+
+	return user
+
+def user_find_by_email (email):
+	return users.find_one ({'email': email})
+
+def user_insert (user):
+	user.id = users.insert ({
+		'email': user.email,
+		'name': user.name,
+		'username': user.username,
+		'password': user.password
+	})
+
+	return user
