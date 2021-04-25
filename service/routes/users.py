9198778a65f9fcb8c030f90569d383cf51720566
ermiry <erick.salas@ermiry.com>
2021-04-24 21:22:48 -0500

    Refactored register handler to use controller

--- service/routes/users.py
+++ service/routes/users.py
@@ -3,10 +3,9 @@ import json
 
 import cerver
 
-import todo
+from errors import *
 import runtime
-
-import models.user
+import todo
 
 import controllers.users
 
@@ -27,25 +26,12 @@ def users_handler (http_receive, request):
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def users_register_handler (http_receive, request):
 	body = cerver.http_request_get_body (request)
-	
-	body_json = json.loads (body.contents.str)
 
-	name = body_json["name"]
-	username = body_json["username"]
-	email = body_json["email"]
-	password = body_json["password"]
-	confirm = body_json["confirm"]
+	result = controllers.users.todo_users_register (body)
 
-	if name is not None and username is not None and email is not None and password is not None and confirm is not None:
-		if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
-			print ("name: ", name)
-			print ("username: ", username)
-			print ("email: ", email)
-			print ("password: ", password)
-			print ("confirm: ", confirm)
-
+	if result == TODO_ERROR_NONE:
 		response = cerver.http_response_json_msg (
-			cerver.HTTP_STATUS_OK, "Users Works!".encode ('utf-8')
+			cerver.HTTP_STATUS_OK, "Created a new user!".encode ('utf-8')
 		)
 
 		if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
@@ -53,17 +39,9 @@ def users_register_handler (http_receive, request):
 
 		cerver.http_response_send (response, http_receive)
 		cerver.http_response_delete (response)
-	
+
 	else:
-		response = cerver.http_response_json_msg (
-			cerver.HTTP_STATUS_BAD_REQUEST, "Missing user values!".encode ('utf-8')
-		)
-
-		if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
-			cerver.http_response_print (response)
-		
-		cerver.http_response_send (response, http_receive)
-		cerver.http_response_delete (response)
+		todo_error_send_response (result, http_receive)
 
 # POST /api/users/login
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
