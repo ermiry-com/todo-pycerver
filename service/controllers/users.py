4b132537dc8307b019f9cabcd96fb35bffb5e6b0
ermiry <erick.salas@ermiry.com>
2021-04-24 21:39:24 -0500

    Added methods to handler user login

--- service/controllers/users.py
+++ service/controllers/users.py
@@ -1,15 +1,25 @@
 import json
+import time
 
 import cerver.utils
 
 from errors import *
+import runtime
+import todo
 
 from models.user import *
+
+def todo_user_get_by_email (email):
+	found = user_get_by_email (email)
+	if (found is not None):
+		return user_parse (found)
+
+	return None
 
 def todo_users_register (body_json):
 	error = TODO_ERROR_NONE
 
-	if body_json is not None:
+	if (body_json is not None):
 		user_values = json.loads (body_json.contents.str)
 
 		email = user_values['email']
@@ -18,9 +28,9 @@ def todo_users_register (body_json):
 		password = user_values['password']
 		confirm = user_values['confirm']
 		
-		if email and name and username and password and confirm:
-			found = user_find_by_email (email)
-			if found is None:
+		if (email and name and username and password and confirm):
+			found = user_get_by_email (email)
+			if (found is None):
 				# if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
 				# 	print ("name: ", name)
 				# 	print ("username: ", username)
@@ -59,3 +69,48 @@ def todo_users_register (body_json):
 		error = TODO_ERROR_MISSING_VALUES
 
 	return error
+
+def todo_user_generate_token (http_receive, user):
+	http_jwt = cerver.http_cerver_auth_jwt_new ();
+	cerver.http_cerver_auth_jwt_add_value_int (http_jwt, "iat".encode ('utf-8'), int (time.time ()));
+	cerver.http_cerver_auth_jwt_add_value (http_jwt, "id".encode ('utf-8'), user.id.encode ('utf-8'));
+	cerver.http_cerver_auth_jwt_add_value (http_jwt, "name".encode ('utf-8'), user.name.encode ('utf-8'));
+	cerver.http_cerver_auth_jwt_add_value (http_jwt, "username".encode ('utf-8'), user.username.encode ('utf-8'));
+	cerver.http_cerver_auth_jwt_add_value (http_jwt, "role".encode ('utf-8'), "common".encode ('utf-8'));
+
+	cerver.http_cerver_auth_generate_bearer_jwt_json (cerver.http_receive_get_cerver (http_receive), http_jwt)
+
+	response = cerver.http_response_create (
+		cerver.HTTP_STATUS_OK,
+		cerver.http_jwt_get_json (http_jwt), cerver.http_jwt_get_json_len (http_jwt)
+	)
+
+	cerver.http_cerver_auth_jwt_delete (http_jwt)
+
+	return response
+
+def todo_users_login (body_json):
+	error = TODO_ERROR_NONE
+	user = None
+
+	if (body_json is not None):
+		user_values = json.loads (body_json.contents.str)
+
+		email = user_values['email']
+		password = user_values['password']
+
+		if (email and password):
+			user = todo_user_get_by_email (email)
+			if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
+				print (user)
+			
+			if (user.password != password):
+				error = TODO_ERROR_BAD_REQUEST
+
+		else:
+			error = TODO_ERROR_MISSING_VALUES
+
+	else:
+		error = TODO_ERROR_MISSING_VALUES
+
+	return error, user
