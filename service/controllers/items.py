5c2fb1a7f759a41b5ef7610c62f473ede11ed59d
ermiry <erick.salas@ermiry.com>
2021-04-24 23:48:22 -0500

    Added todo_item_update () in items controller

--- service/controllers/items.py
+++ service/controllers/items.py
@@ -41,6 +41,37 @@ def todo_item_create (user, body_json):
 
 	return error
 
+def todo_item_update (item_id_str, user, body_json):
+	error = TODO_ERROR_NONE
+
+	if (body_json is not None):
+		item_values = json.loads (body_json.contents.str)
+
+		title = item_values['title']
+		description = item_values['description']
+
+		if (title and description):
+			item = todo_item_get_by_id_and_user (
+				item_id_str.contents.str.decode ("utf-8"), user
+			)
+
+			if (item):
+				item.title = title
+				item.description = description
+				if (item_update (item) is False):
+					error = TODO_ERROR_SERVER_ERROR
+
+			else:
+				error = TODO_ERROR_BAD_REQUEST
+			
+		else:
+			error = TODO_ERROR_MISSING_VALUES
+
+	else:
+		error = TODO_ERROR_MISSING_VALUES
+
+	return error
+
 def todo_item_delete_by_id_and_user (item_id_str, user):
 	return item_delete_by_id_and_user (
 		item_id_str.contents.str.decode ("utf-8"), user.id
