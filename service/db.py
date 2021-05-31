503a63fd4f2fba72a67751ac7e39fdcc6a0ab9c0
ermiry <erick.salas@ermiry.com>
2021-05-31 09:48:52 -0500

    Added extra check for successful db connection

--- service/db.py
+++ service/db.py
@@ -5,25 +5,36 @@ import cerver.utils
 
 import todo
 
-from models.user import user_model_init
 from models.item import item_model_init
+from models.user import user_model_init
 
 todo_db = None
 
 def todo_mongo_init ():
 	global todo_db
 
+	result = False
+
 	client = MongoClient (todo.MONGO_URI)
 	try:
 		todo_db = client.todo
+
+		todo_db.command ('ping')
+
 		cerver.utils.cerver_log_success (
 			"Mongo DB connected!".encode ("utf-8")
 		)
 
-		user_model_init (todo_db)
 		item_model_init (todo_db)
+		user_model_init (todo_db)
 
-	except:
+		result = True
+
+	except Exception as e:
 		cerver.utils.cerver_log_error (
 			"Error connecting to Mongo DB".encode ("utf-8")
 		)
+
+		print (e)
+
+	return result
